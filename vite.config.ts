663ecdb863fec4ce2564f858655c341d49e9d0da
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-02-25 11:07:22 +0530

    chore: Add support for path alias & node type defs
    - Add path alias for `components`
    - Add path alias support in vite config
    - Add dep "@types/node"

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, AliasOptions } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import * as tsconfig from './tsconfig.json';
+
+// @src: https://github.com/Melvin-Abraham/Google-Assistant-Unofficial-Desktop-Client/blob/next/vite.config.ts#L9
+// Resolve path aliases from `tsconfig.json`
+const tsconfigAliases = tsconfig.compilerOptions.paths;
+const aliasOptions: AliasOptions = {};
+
+Object.entries(tsconfigAliases).forEach((aliasEntry) => {
+  const [_alias, paths] = aliasEntry;
+
+  // Strip glob from the end of path
+  // Ex: "path/*" -> "path"
+
+  const pathEndGlobRegex = /[/*]*$/;
+  const alias = _alias.replace(pathEndGlobRegex, '');
+  const resolutionPath = resolve(
+    __dirname,
+    paths[0].replace(pathEndGlobRegex, ''),
+  );
+
+  aliasOptions[alias] = resolutionPath;
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: aliasOptions,
+  }
+});
