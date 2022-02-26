49a336c910a0709f76c72f202582095b0382d311
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-02-26 16:19:12 +0530

    chore: Mark dummy todo item "Eat" as completed

--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -1,7 +1,7 @@
 import { TodoItem } from './types';
 
 const dummyData: TodoItem[] = [
-  { id: '1', todo: 'Eat', complete: false },
+  { id: '1', todo: 'Eat', complete: true },
   { id: '2', todo: 'Sleep', complete: false },
   { id: '3', todo: 'Repeat', complete: false },
 ]
